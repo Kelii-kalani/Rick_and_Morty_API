ba9b33d0b6a6c3dc1511d813ba5530b7c3d435b2
Michael <keliikalani87@gmail.com>
2023-03-15 13:08:38 -0700

    8th commit

--- Controllers/RickAndMortyController.cs
+++ Controllers/RickAndMortyController.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rick_and_Morty_API.Models;
 using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Rick_and_Morty_API.Controllers
 {
+    
     public class RickAndMortyController : Controller
     {
         public IActionResult Index([FromQuery(Name = "resource_url")] string resourceUrl = "https://rickandmortyapi.com/api/character")
         {
-            if (resourceUrl == "https://rickandmortyapi.com/api/character")
+            if (resourceUrl.StartsWith("https://rickandmortyapi.com"))
             {
                 var client = new HttpClient();
 
